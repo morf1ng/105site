a501d38375086b706403841def9ab6ff36f5dce8
morf1ng <ali.yagiyaev@yandex.ru>
2026-03-23 13:26:01 +0300

    GHKLJGA

--- front/next.config.ts
+++ front/next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   env: {
-    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000',
-    NEXT_PUBLIC_API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000',
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL || 'http://localhost:8000',
   },
 };
 
